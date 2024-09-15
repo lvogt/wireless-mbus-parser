a8de30e9e1420c112550f60df74cb5bad349f10e
Christian Landvogt <chris@lvgt.de>
2024-09-16 00:05:24 +0200

    Initial commit
    * typescript
    * tsup
    * eslint+prettier
    * vitest

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}", "tsup.config.ts", "vitest.config.ts"] },
+  { ignores: ["node_modules", "dist/**"] },
+  { languageOptions: { globals: globals.node } },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+  },
+  {
+    rules: {
+      "no-restricted-imports": ["error", { patterns: ["\\..*"] }],
+    },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+];
