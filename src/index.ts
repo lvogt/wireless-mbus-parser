4ab9296e4e5570f0ecaff7f7c6c9b1940261d91b
Christian Landvogt <chris@lvgt.de>
2024-10-17 22:31:15 +0200

    Export types and classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
 export { stripAnyCrc } from "@/crc/crcHandler";
+
+export { WirelessMbusParser } from "@/parser/parser";
+
+export type {
+  WiredLinkLayer,
+  LinkLayer,
+  MeterData,
+  ParserResult,
+  FullParserResult,
+  DataRecord,
+  DataRecordHeader,
+  DataType,
+  EvaluatedData,
+  DataInformationBlock,
+  PrimaryVif,
+  PrimaryVifNumber,
+  PrimaryVifString,
+  ValueInformationBlock,
+  Config,
+  ConfigMode5,
+  ConfigMode7,
+  ConfigMode13,
+  ApplicationLayer,
+  ApplicationLayer0,
+  ApplicationLayer4,
+  ApplicationLayer12,
+  ApplicationLayerCompact,
+  ApplicationLayerDummy,
+  AuthenticationAndFragmentationLayer,
+  ExtendedLinkLayer,
+  ExtendedLinkLayer2,
+  ExtendedLinkLayer10,
+  ExtendedLinkLayer8,
+  ExtendedLinkLayer16,
+} from "@/types";
+
+export { EvaluatedDataType, VifTable } from "@/types";
+
+export type { ErrorName, ParserError } from "@/helper/error";
